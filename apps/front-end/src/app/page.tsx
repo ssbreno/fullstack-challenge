34a8142937a44dcd4232e6b650d26613d151b0c2
Breno Sobral <ssobralbreno@gmail.com>
2024-05-21 11:00:19 -0300

    feat: add search by id in template

--- apps/front-end/src/app/page.tsx
+++ apps/front-end/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { TableWidget } from '@/components/table';
 import { EmployeeDTO } from '@/dtos/employee.dto';
-import { deleteEmployee, getEmployees } from '@/services/employee';
+import { deleteEmployee, getEmployees, getEmployee } from '@/services/employee';
 import { Box, Button, Card, CardBody, CardHeader, Container, Heading, Input, useToast } from '@chakra-ui/react';
 import { useRouter } from 'next/navigation';
 import { useEffect, useMemo, useState } from 'react';
@@ -10,6 +10,7 @@ import { useEffect, useMemo, useState } from 'react';
 export default function Home() {
   const [employees, setEmployees] = useState<EmployeeDTO[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const router = useRouter();
   const toast = useToast();
@@ -67,6 +68,35 @@ export default function Home() {
     }
   }
 
+  const handleSearchEmployee = async (id: string) => {
+    setIsLoading(true);
+    try {
+      const response = await getEmployee(id);
+      setIsLoading(false);
+      setEmployees(response.data ? [response.data] : []);
+    } catch (err) {
+      setIsLoading(false);
+      setEmployees([]);
+      toast({
+        title: "Erro ao buscar funcionário.",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  }
+
+
+  const handleKeyDown = (event: { key: string; }) => {
+    if (event.key === 'Enter') {
+      handleSearchEmployee(searchTerm);
+    }
+  }
+
+  const handleBlur = () => {
+    handleSearchEmployee(searchTerm);
+  }
+
   useEffect(() => {
     handleEmployees();
   }, [])
@@ -81,7 +111,16 @@ export default function Home() {
         </CardHeader>
         <CardBody>
           <Box display="flex" gap="2" justifyContent="space-between" mb="4">
-            <Input maxW="320px" placeholder="Buscar funcionário" size="md" rounded="xl" />
+            <Input
+              maxW="320px"
+              placeholder="Buscar funcionário"
+              size="md"
+              rounded="xl"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              onKeyDown={handleKeyDown}
+              onBlur={handleBlur}
+            />
             <Button colorScheme="blue" size="md" rounded="xl" onClick={handleNewEmployee}>Novo Funcionário</Button>
           </Box>
           <TableWidget 
